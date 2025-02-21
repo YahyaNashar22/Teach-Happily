73c9034be69451d83b7822a90711ff41f04ac8f8
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-22 01:43:12 +0200

    db config

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from 'body-parser';
+
+import databaseConnection from "./db/databaseConnection.js";
+
+
+// Declaration
+dotenv.config();
+const app = express();
+
+// CORS Policies
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
+));
+
+// Configuration Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+
+// Routes / APIs
+
+
+// Connect to server
+app.listen(process.env.PORT, (error) => {
+    if (!error) {
+        console.log(`Server Running On Port: ${process.env.PORT}`);
+    } else {
+        console.log("Couldn't Connect To Server!");
+        console.error(`Error: ${error}`);
+    }
+});
+databaseConnection();
