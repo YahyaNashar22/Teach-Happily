3728ca7a9572e2cc8c353433d631b5be864c90eb
YahyaNashar22 <yahyanashar22@gmail.com>
2025-03-09 20:37:56 +0200

    time for backend

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -1,6 +1,7 @@
 import ContactCards from "../components/ContactCards";
 import ContactUsHero from "../components/ContactUsHero";
 import Map from "../components/Map";
+import SocialMedia from "../components/SocialMedia";
 
 const ContactPage = () => {
   return (
@@ -8,6 +9,7 @@ const ContactPage = () => {
       <ContactUsHero />
       <ContactCards />
       <Map />
+      <SocialMedia />
     </main>
   );
 };
