ba4ed24ab8eb1639e65cced98310a42f15c00663
YahyaNashar22 <yahyanashar22@gmail.com>
2025-03-15 03:30:29 +0200

    cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 // CORS Policies
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://teach-happily.onrender.com"],
     credentials: true,
     optionsSuccessStatus: 200,
 }
