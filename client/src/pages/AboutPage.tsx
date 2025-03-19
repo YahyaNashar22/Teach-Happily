230076a4efa63afba2e288e7b45ac11a2f672dde
YahyaNashar22 <yahyanashar22@gmail.com>
2025-03-19 11:54:57 +0200

    removed educational level

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,6 +1,5 @@
 import AboutUsBlog from "../components/AboutUsBlog";
 import AboutUsHero from "../components/AboutUsHero";
-import EducationLevel from "../components/EducationLevel";
 import HomeNumbers from "../components/HomeNumbers";
 import SocialMedia from "../components/SocialMedia";
 import Testimonials from "../components/Testimonials";
@@ -10,7 +9,6 @@ const AboutPage = () => {
     <main>
       <AboutUsHero />
       <AboutUsBlog />
-      <EducationLevel />
       <HomeNumbers />
       <Testimonials />
       <SocialMedia />
