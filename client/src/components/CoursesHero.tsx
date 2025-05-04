44df2d993a371940c3c9c2634b63be4b61221b14
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-04 14:27:26 +0300

    courses page

--- client/src/components/CoursesHero.tsx
+++ client/src/components/CoursesHero.tsx
@@ -1,13 +1,28 @@
 import "../css/CoursesHero.css";
 
+import magnifier from "../assets/Website design-09.png";
+
 const CoursesHero = () => {
   return (
     <section className="courses-hero">
-      <h1 className="courses-hero-title">الدورات</h1>
-      {/* <p className="courses-hero-slogan">
-        جعل التعلم والتدريس أكثر فعالية من خلال المشاركة النشطة والتعاون بين
-        الطلاب
-      </p> */}
+      <div className="courses-content">
+        <h1 className="courses-title">الدورات</h1>
+
+        <p className="courses-purple-text">
+          لأنّ التّعليم رحلة ممتعة، نقدّم لك منصّة و خبرة تعليميّة
+        </p>
+
+        <p className="courses-yellow-text"> تفوق 10 سنوات</p>
+
+        <p className="courses-purple-text">لجعل التّدريس متعة لك ولطلّابك.</p>
+      </div>
+      <img
+        src={magnifier}
+        alt="magnifier"
+        loading="lazy"
+        className="courses-magnifier"
+        width={500}
+      />
     </section>
   );
 };
--- client/src/pages/CoursesPage.tsx
+++ client/src/pages/CoursesPage.tsx
@@ -1,9 +1,7 @@
-
 import Courses from "../components/Courses";
 import CoursesHero from "../components/CoursesHero";
 
 const CoursesPage = () => {
- 
   return (
     <main>
       <CoursesHero />
