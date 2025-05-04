4e32d183ae004bc43c2737366f54a1665089d21b
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-04 12:29:22 +0300

    about us hero

--- client/src/components/AboutUsHero.tsx
+++ client/src/components/AboutUsHero.tsx
@@ -1,12 +1,28 @@
 import "../css/AboutUsHero.css";
 
+import magnifier from "../assets/Website design-10.png";
+
 const AboutUsHero = () => {
   return (
     <section className="about-us-hero">
-      <h1 className="about-us-hero-title">معلومات عنا</h1>
-      {/* <p className="about-us-hero-slogan">
-        إن قوة شركتنا سمحت لنا بأن نكون دائمًا متواجدين لمساعدة عملائنا
-      </p> */}
+      <div className="about-us-content">
+        <h1 className="about-us-title">علّم بسعادة</h1>
+
+        <p className="about-us-purple-text">
+          لأنّ التّعليم رحلة ممتعة، نقدّم لك منصّة و خبرة تعليميّة
+        </p>
+
+        <p className="about-us-yellow-text"> تفوق 10 سنوات</p>
+
+        <p className="about-us-purple-text">لجعل التّدريس متعة لك ولطلّابك.</p>
+      </div>
+      <img
+        src={magnifier}
+        alt="magnifier"
+        loading="lazy"
+        className="about-us-magnifier"
+        width={500}
+      />
     </section>
   );
 };
